f3b2b3e39a29b08abba35852e29446e5fb487394
Dan Capps <dcapps@mail.greenriver.edu>
2018-03-10 13:25:33 -0800

    C++ code for displaying safest driving area

--- main.cpp
+++ main.cpp
@@ -1,89 +1,55 @@
 #include <iostream>
-#include <iomanip>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-void getShippingData(int &, int &, double &);
-void displayOrderStatus(int, int, double, double = 10.0);
+//Prototypes
+int getNumAccidents(string);
+void findLowest(int, int, int, int, int);
 
 int main() {
+    //Define variables
+    string north, south, east, west, central;
+    int accidentsInNorth, accidentsInSouth, accidentsInEast, accidentsInWest, accidentsInCentral;
+
+    getNumAccidents(north);
+    getNumAccidents(south);
+    getNumAccidents(east);
+    getNumAccidents(west);
+    getNumAccidents(central);
 
 
+    //findLowest(accidentsInNorth, accidentsInSouth, accidentsInEast, accidentsInWest, accidentsInCentral);
 
     //Display console
     system("pause");
     return 0;
 }
 
-void getShippingData(int &numberSpoolsOrdered, int &numberSpoolsInStock, double &numberSpecialCharges)
+int getNumAccidents(string region)
 {
-    char extraShippingCharges;
+    int accidents;
 
-    cout << "Please enter the number of spools ordered to be shipped: ";
-    cin >> numberSpoolsOrdered;
-    while (numberSpoolsOrdered < 1)
+    //Ask user for the number of automobile accidents reported in that year
+    cout << "Enter the number of automobile accidents reported in that year for the "<< region << " : " << endl;
+    cin >> accidents;
+
+    //Validate input
+    while(accidents < 0)
     {
-        cout << "The number you have entered is invalid." << endl;
-        cout << "Please enter the number of spools ordered to be shipped: ";
-        cin >> numberSpoolsOrdered;
+        cout << "I cannot accept a number lower than 0" << endl;
+        cout << "Enter the number of automobile accidents reported in that year: " << endl;
+        cin >> accidents;
     }
 
-    cout << "Please enter the number of spools in stock: ";
-    cin >> numberSpoolsInStock;
-    while(numberSpoolsInStock < 0)
-    {
-        cout << "The number you have entered is invalid." << endl;
-        cout << "Please enter the number of spools in stock: ";
-        cin >> numberSpoolsInStock;
-    }
+    //return input
 
-    cout << "Are there any special shipping and handling charges for this order? Please enter Y for yes or N for no: ";
-    cin >> extraShippingCharges;
-    if (extraShippingCharges == 'Y' || extraShippingCharges == 'y')
-    {
-        cout << "Please enter the amount of the special shipping and handling charges: ";
-        cin >> numberSpecialCharges;
-        while(numberSpecialCharges < 0)
-        {
-            cout << "The number you have entered is invalid" << endl;
-            cout << "Please enter the amount of the special shipping and handling charges: ";
-            cin >> numberSpecialCharges;
-        }
-    }
+    return accidents;
+
 }
 
-void displayOrderStatus(int spoolOrdered, int spoolInStock, double spoolSpecialCharges, double spoolCharges)
+void findLowest(int accidentRegion1, int accidentRegion2, int accidentRegion3, int accidentRegion4, int accidentRegion5)
 {
-    const double SPOOL_UNIT_COST = 100.00;
-    int spoolBackOrder = 0;
-    double spoolSubTotal = 0.0, spoolTotal = 0.0;
-
-    if(spoolOrdered > spoolInStock)
-    {
-        spoolBackOrder = (spoolOrdered - spoolInStock);
-        spoolSubTotal = (spoolOrdered - spoolBackOrder) * SPOOL_UNIT_COST;
-
-        if (spoolSpecialCharges)
-        {
-            spoolTotal = (spoolOrdered - spoolBackOrder) * SPOOL_UNIT_COST + (spoolOrdered - spoolBackOrder) * spoolCharges + spoolSpecialCharges;
-        }
-        else
-        {
-            spoolTotal = (spoolOrdered - spoolBackOrder) * SPOOL_UNIT_COST + (spoolOrdered - spoolBackOrder) * spoolCharges;
-        }
-    }
-    else if (spoolOrdered < spoolInStock)
-    {
-        spoolSubTotal = (spoolOrdered * SPOOL_UNIT_COST);
-        if(spoolSpecialCharges)
-        {
-            spoolTotal = (spoolOrdered * SPOOL_UNIT_COST) + (spoolOrdered * spoolCharges) + spoolSpecialCharges;
-        }
-        else
-        {
-            spoolTotal = (spoolOrdered * SPOOL_UNIT_COST) + (spoolOrdered * spoolCharges);
-        }
-    }
-
 
 }
